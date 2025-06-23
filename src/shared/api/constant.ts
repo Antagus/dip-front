c00ee389d3ee3face815bbb0760bbb73ac83eeda
Antagus <good.anat@yandex.ru>
2025-06-23 04:41:15 +0300

    fix

--- src/shared/api/constant.ts
+++ src/shared/api/constant.ts
@@ -1,5 +1,5 @@
 export const PORT_SERVER = 3222;
-export const ADDRESS_SERVER = "localhost";
+export const ADDRESS_SERVER = "89.111.169.232";
 
 export const getPathAPI = (path: string) => {
   return `http://${ADDRESS_SERVER}:${PORT_SERVER}${path}`;
